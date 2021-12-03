b54a52254d727459445b1358d0d08ec0937d1630
Simon-Martin Schröder <martin.schroeder@nerdluecht.de>
2021-12-03 16:55:09 +0100

    Transform STML files to dict

--- silverturtle/parse.py
+++ silverturtle/parse.py
@@ -1,10 +1,10 @@
 import re
-from typing import List, Optional, Tuple
+from typing import Iterable, List, Optional, Tuple
 import itertools
 import textwrap
 
 
-class Block(list):
+class Block(List["Expression"]):
     def __init__(self, it=None, *, parent: Optional["Block"] = None, indent=None):
         if it is None:
             super().__init__()
@@ -55,11 +55,15 @@ class Blank(Expression):
 
 
 class Node(Expression):
-    regex = re.compile(r"(\w+)::$")
+    regex = re.compile(r"([a-zA-Z0-9_-]+)::$")
 
 
 class Tag(Expression):
-    regex = re.compile(r"(\w+)~=(.*)$")
+    regex = re.compile(r"([a-zA-Z0-9:_.-]+)([*]?)\s*~=\s*(.*)$")
+
+
+class Meta(Expression):
+    regex = re.compile(r"([a-zA-Z0-9:_.-]+)\s*=\s*(.*)$")
 
 
 class Alias(Expression):
@@ -81,10 +85,12 @@ class ParserError(Exception):
     pass
 
 
-def gen_ast(source: str):
+def gen_ast(lines: Iterable[str]):
     root = Block()
     current_block: Block = root
-    for line_no, line in enumerate(source.splitlines()):
+    for line_no, line in enumerate(lines, start=1):
+        line = line.rstrip()
+
         indent, content = split_line(line)
 
         if current_block.indent is None:
@@ -96,8 +102,17 @@ def gen_ast(source: str):
         # Indentation is larger than previously
         elif indent.startswith(current_block.indent):
             # Check if previous token was a Node
-            if not isinstance(current_block[-1], Node):
-                raise ParserError(f"Unexpected indent in line {line_no}: {line!r}")
+            prev = None
+            for prev in reversed(current_block):
+                if isinstance(prev, Blank):
+                    pass
+                else:
+                    break
+
+            if not isinstance(prev, Node):
+                raise ParserError(
+                    f"Unexpected indent in line {line_no}: {line!r}\nPrevious non-empty token was {prev}."
+                )
 
             b = Block(parent=current_block, indent=indent)
             current_block.append(b)
@@ -128,6 +143,7 @@ def gen_ast(source: str):
             or Tag.try_create(content, current_block)
             or Alias.try_create(content, current_block)
             or Blank.try_create(content, current_block)
+            or Meta.try_create(content, current_block)
         ):
             raise ParserError(f"Can not parse line {line_no}: {line!r}")
 
@@ -152,11 +168,40 @@ def build_node(node: Node, block: Optional[Block] = None):
     pass
 
 
-def gen_dict(source: str):
-    root = gen_ast(source)
+def ast2dict(block: Block, data=None):
+    if data is None:
+        data = {}
+    doc = []
+    child = None
+    for expr in block:
+        if type(expr) == Comment:
+            doc.append(expr.args[0])
+            continue
+        elif type(expr) == Blank:
+            if doc and "doc" not in data:
+                data["doc"] = "\n".join(doc)
+        elif type(expr) == Node:
+            child = data.setdefault("children", {})[expr.args[0]] = {}
+        elif type(expr) == Block:
+            ast2dict(expr, child)
+        elif type(expr) == Alias:
+            data.setdefault("aliases", []).append(expr.args[0])
+        elif type(expr) == Tag:
+            tag = data.setdefault("tags", {})[expr.args[0]] = {
+                "pattern": expr.args[2],
+                "multi": expr.args[1] == "*",
+            }
+            if doc:
+                tag["doc"] = "\n".join(doc)
+        elif type(expr) == Meta:
+            data.setdefault("meta", {})[expr.args[0]] = expr.args[1]
+        else:
+            raise ParserError(f"Unexpected expression: {expr!r}")
 
-    # Root needs to be a block with exactly one node with empty name, optionally followed by a block
-    if len
+        # Reset doc
+        doc = []
+
+    return data
 
 
 def compare_blocks(block1: Block, block2: Block):
@@ -172,3 +217,30 @@ def compare_blocks(block1: Block, block2: Block):
                 yield f"[fail] {expr1!r} != {expr2!r}"
 
     return "\n".join(_comparison())
+
+
+def format_block(block: Block):
+    def _comparison():
+        for expr in block:
+            if type(expr) == Block:
+                yield textwrap.indent(format_block(expr), "  ")
+            else:
+                yield repr(expr)
+
+    return "\n".join(_comparison())
+
+
+if __name__ == "__main__":
+    import argparse
+    from pprint import pprint
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument("taxonomy_fn")
+    args = parser.parse_args()
+
+    with open(args.taxonomy_fn) as f:
+        ast = gen_ast(f)
+
+    print(format_block(ast))
+
+    pprint(ast2dict(ast))
